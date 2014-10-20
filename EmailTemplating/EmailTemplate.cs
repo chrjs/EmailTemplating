61b8fa199c92f832debe6b6cc3f2ca2a28656110
Christian Salvaneschi <chrjs75@gmail.com>
2014-10-21 01:22:55 +0200

    Initial check-in of module EmailTemplating

---
+++ EmailTemplating/EmailTemplate.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Reflection;
+
+namespace EmailTemplating
+{
+  internal class Placeholder
+  {
+    public Placeholder(string placeholder)
+    {
+      this.PlaceholderString = placeholder;
+      placeholder = placeholder.Replace("@", "");
+
+      string[] fields = placeholder.Split(":".ToCharArray());
+
+      this.Path = fields[0];
+
+      if (fields.Length == 2)
+      {
+        this.Format = fields[1];
+      } 
+      else if (fields.Length > 2)
+      {
+        throw new ApplicationException("Invalid placeholder format: " + placeholder);
+      }
+
+      this.Fields = this.Path.Split(".".ToCharArray());
+      this.RootObjectKey = this.Fields[0];
+    }
+
+    public string RootObjectKey { get; private set; }
+    public string Path { get; private set; }
+    public string Format { get; private set; }
+    public string[] Fields { get; private set; }
+    public string PlaceholderString { get; private set; }
+  }
+
+  public class EmailTemplate
+  {
+    public EmailTemplate()
+    {
+    }
+
+    private Dictionary<string, object> placeholders
+    {
+      get;
+      set;
+    }
+
+    public string GetEmailBody(string templateFilePath)
+    {
+      return this.GetEmailBody(templateFilePath, null, null);
+    }
+
+    public string GetEmailBody(string templateFilePath, Encoding encoding)
+    {
+      return this.GetEmailBody(templateFilePath, null, encoding);
+    }
+
+    public string GetEmailBody(string templateFilePath, string masterTemplateFilePath)
+    {
+      return this.GetEmailBody(templateFilePath, masterTemplateFilePath, new UTF8Encoding());
+    }
+
+    public string GetEmailBody(string templateFilePath, string masterTemplateFilePath, Encoding encoding)
+    {
+      string template = File.ReadAllText(templateFilePath, encoding);
+      template = ProcessPlaceholders(template);
+
+      if (String.IsNullOrEmpty(masterTemplateFilePath))
+        return template;
+
+      string mastertemplate = File.ReadAllText(masterTemplateFilePath, encoding);
+      mastertemplate = ProcessPlaceholders(mastertemplate);
+      return mastertemplate.Replace("@template@", template);
+    }
+
+    public void AddDataObject(string code, object dataObject)
+    {
+      if (this.placeholders == null)
+        this.placeholders = new Dictionary<string, object>();
+
+      this.placeholders.Add(code, dataObject);
+    }
+
+    #region Protected methods
+    private string ProcessPlaceholders(string template)
+    {
+      Regex regex = new Regex("@(.)*@");
+
+      MatchCollection matches = regex.Matches(template);
+      foreach (Match match in matches)
+      {
+        Placeholder placeholder = new Placeholder(match.Value);
+
+        if (this.placeholders.ContainsKey(placeholder.RootObjectKey))
+        {
+          string value = this.GetObjectPropertyValue(this.placeholders[placeholder.RootObjectKey], placeholder, 1);
+          template = template.Replace(placeholder.PlaceholderString, System.Net.WebUtility.HtmlEncode(value));
+        }
+        else
+        {
+          throw new InvalidDataException("Placeholder \"" + placeholder.PlaceholderString + "\" not found in data object collection.");
+        }
+      }
+
+      return template;
+    }
+
+    private string GetObjectPropertyValue(object dataObject, Placeholder placeholder, int index)
+    {
+      if (index >= placeholder.Fields.Length)
+      {
+        throw new ArgumentOutOfRangeException("index");
+      }
+
+      string propertyName = placeholder.Fields[index];
+      PropertyInfo p = dataObject.GetType().GetProperty(propertyName);
+      if (p == null)
+      {
+        new ApplicationException("Field named \"" + propertyName + "\" not found.");
+      }
+
+      object value = p.GetValue(dataObject);
+
+      if (index == placeholder.Fields.Length - 1)
+      {
+        if (!String.IsNullOrEmpty(placeholder.Format))
+        {
+          if (value is DateTime)
+            return ((DateTime)value).ToString(placeholder.Format);
+          else
+            return String.Format(placeholder.Format, value);
+        } else
+        {
+          return value.ToString();
+        }
+      } 
+      else
+      {
+        return this.GetObjectPropertyValue(value, placeholder, index + 1);
+      }
+    }
+    #endregion
+  }
+}
+
---
+++ EmailTemplatingTest/Test.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using EmailTemplating;
+using System.Reflection;
+using System.IO;
+
+namespace EmailTemplatingTest
+{
+  public class DataClass
+  {
+    public string ValString
+    {
+      get { return "Pippo"; }
+    }
+
+    public int ValInt
+    {
+      get { return 100; }
+    }
+
+    public DateTime ValDateTime
+    {
+      get { return DateTime.UtcNow; }
+    }
+  }
+
+  [TestFixture()]
+  public class Test
+  {
+    [Test()]
+    public void TestCase()
+    {
+      EmailTemplate et = new EmailTemplate();
+      et.AddDataObject("MyData", new DataClass());
+      File.WriteAllText("email-body.html", et.GetEmailBody("template1.html", "master.html"));
+    }
+  }
+}
+
